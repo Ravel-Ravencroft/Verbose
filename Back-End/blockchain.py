3e85e82fe20223a12cef341bca31e9c44f88b913
Michael Nadesapillai <54156521+Ravel-Ravencroft@users.noreply.github.com>
2021-04-19 03:08:22 +0530

    Improvements to Functionality
    - Optimised JSON returning for improved usability.
    - Split `timestamp` Attribute of Block into `date` and `time`
    - Added `generate_day_list()` in preparation for the improved PDF Generator functionality.

--- Back-End/blockchain.py
+++ Back-End/blockchain.py
@@ -1,37 +1,61 @@
-from datetime import datetime
+from datetime import date, datetime
 from hashlib import sha256
 
 class Block:
-    def __init__(self, student_id, previous_hash, timestamp):
+    def __init__(self, student_id, date, time, previous_hash):
         self.student_id = student_id
+        self.date = date
+        self.time = time
         self.previous_hash = previous_hash
-        self.timestamp = timestamp
 
     def compute_hash(self):
         return sha256( self.compute_hash_string().encode() ).hexdigest()
 
     def compute_hash_string(self):
-        return '{} {} - {}'.format(self.timestamp, self.student_id, self.previous_hash)
+        return '{} {} - {}'.format(self.date + " " + self.time, self.student_id, self.previous_hash)
 
-    def to_string(self):
-        return ( {'id': self.student_id, 'date': self.timestamp[0:10], 'time': self.timestamp[11:]} )
+    def parse_json(self):
+        return {"id": self.student_id, "date": self.date, "time": self.time, "hash": self.previous_hash}
 
 class Blockchain:
     DEFAULT_HASH = sha256( "The Default Hash".encode() ).hexdigest()
     chain = []
 
-    def add_block(self, student_id, previous_hash = DEFAULT_HASH if not chain else chain[-1].compute_hash(), timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')):
-        self.chain.append( Block(student_id, previous_hash, timestamp) )
+    def add_block(self, student_id, previous_hash, timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')):
+        self.chain.append( Block(student_id, timestamp.split(" ")[0], timestamp.split(" ")[1], previous_hash) )
 
-    def generate_json(self, student_id = None):
+    # Rename to generate_student_list
+    def generate_student_list(self, student_id = None, date = None):
         data = []
         for block in self.chain:
             if student_id is not None and block.student_id != student_id:
                 continue
+            if date is not None and block.date != date:
+                continue
 
-            data.append( block.to_string() )
+            data.append( {'date': block.date, 'time': block.time} )
 
         return data
+
+    def generate_day_list( self, date = date.today().strftime('%Y-%m-%d'), ids = None, json = True ):
+        data = []
+        
+        for block in reversed(self.chain):
+            if block.date != date:
+                continue
+            if ids is not None and block.student_id not in ids:
+                continue
+
+            data.append( {'id': block.student_id, 'time': block.timestamp[11:]} )
+            ids.remove(block.student_id)
+
+        for id in ids:
+            data.append( {'id': id, 'time': None} )
+
+        if json:
+            return {'date': date, 'details': data}
+        else:
+            return data
 
     def print_blockchain(self):
         print("\nThe Blockchain")
