8ccd0a676d6e406b25b3f1d576501ca255ffff8b
Linukagoonetilleka <66565164+Linukagoonetilleka@users.noreply.github.com>
2021-04-22 12:28:13 +0530

    Updated Formatting and Added JSON Compatibility
    - Improved the formatting of the report
    - Added functionality to parse the JSON Array sent from the server.

--- Back-End/pdf_generator.py
+++ Back-End/pdf_generator.py
@@ -1,66 +1,60 @@
-# dummy data
-data = [
-    ['Student ID','Date and Time','Attendance'],
-    ['w179669','13.3.2021 8am','absent'],
-    ['w179455','21.4.2021 5pm','late'],
-    ['w179187','5.11.2021 2pm','present'],
-    ['w179423','6.9.2021 6am','late']
-]
+from datetime import datetime
 
-# creating a new pdf
-fileName = 'pdfTable.pdf'
-
-from reportlab.platypus import SimpleDocTemplate
-from reportlab.lib.pagesizes import letter
-
-pdf = SimpleDocTemplate(
-    fileName,
-    pagesize=letter
-)
-# creating a table
-from reportlab.platypus import Table
-table = Table(data)
-
-# styling the table
-from reportlab.platypus import TableStyle
 from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.platypus import Frame, Table, TableStyle
 
-style = TableStyle([
-    # main row styles
-    ('BACKGROUND', (0,0), (2,0), colors.mediumslateblue),
-    ('TEXTCOLOR', (0,0), (-1,0), colors.black),
-    ('ALIGN', (0,0), (-1,-1), 'CENTER'),
-    ('FONTSIZE', (0,0), (-1,0), 16),
-    ('FONTNAME', (0,0), (-1,-1), 'Courier-Bold'),
-    ('BOTTOMPADDING', (0,0), (-1,0), 12),
-    ('BACKGROUND', (0,1), (-1,-1), colors.lightskyblue),
-])
-table.setStyle(style)
+from config import INSTITUTE_START_TIME as time
 
-# generating different colored rows
-rowNumber = len(data)
-for i in range(1, rowNumber):
-    if i % 2 == 0:
-        backgroundcolor = colors.deepskyblue
-    else:
-        backgroundcolor = colors.lightskyblue
 
-    tablestyle1 = TableStyle(
-        [('BACKGROUND', (0,i), (-1,i), backgroundcolor)]
-    )
-    table.setStyle(tablestyle1)
+PAGE_COUNT = 15
 
-# borders
-tablestyle2 = TableStyle(
-    [
-        ('BOX', (0,0), (-1,-1), 2, colors.black),
-        ('GRID', (0,1), (-1,-1), 2, colors.black),
-    ]
-)
-table.setStyle(tablestyle2)
+def create_pdf(id, data):
+    today = datetime.now()
 
-# passing the elements to the table
-elements = []
-elements.append(table)
+    canvas = Canvas("Student Daily Attendance Records.pdf", pagesize = A4)
 
-pdf.build(elements)
+    i = 0
+    page = 0
+    while i < len(data):
+        if i%PAGE_COUNT == 0:
+            canvas.setFont("Times-Bold", 30)
+            canvas.drawString(210, 750, "Attendance List")
+
+            canvas.setFont("Times-Roman", 16)
+            canvas.drawString(50, 700, "Teacher ID: " + id)
+            canvas.drawString( 50, 675, "Date Generated: " + today.strftime("%Y-%m-%d") )
+            canvas.drawString( 50, 650, "Time Generated: " + today.strftime("%H:%M:%S") )
+
+            rows = []
+            rows.append(['Student ID', 'Check-In Time', 'Punctuality'])
+            page+=1
+
+        punctuality = "Absent" if data[i]['time'] == "-" else "On Time" if data[i]['time'] <= time else "Late"
+        rows.append([data[i]['id'], data[i]['time'], punctuality])
+
+        if i == (len(data)-1) or i == ( (page*PAGE_COUNT)-1 ):
+            table = Table(rows, 150, 30)
+
+            tableStyle = TableStyle([
+                ('BACKGROUND', (0, 0), (2, 0), colors.deepskyblue),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTSIZE', (0, 0), (-1, 0), 20),
+                ('FONT', (0, 0), (-1, -1), 'Helvetica-Bold'),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 16),
+                ('BACKGROUND', (0, 1), (-1, -1), colors.whitesmoke),
+                ('BOX', (0, 0), (-1, -1), 2, colors.black),
+                ('GRID', (0, 0), (-1, -1), 2, colors.black),
+            ])
+
+            table.setStyle(tableStyle)
+            frame = Frame(50, 0, 500, 625)
+            frame.addFromList([table], canvas)
+            canvas.drawString( 525, 25, "Page " + str(page) )
+            canvas.showPage()
+
+        i+=1
+
+    canvas.save()
