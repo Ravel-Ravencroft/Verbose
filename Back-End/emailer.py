603633da116751849086d0072450ca757e87b79a
YaishRiaz <66737404+YaishRiaz@users.noreply.github.com>
2021-04-23 03:48:35 +0530

    Updates and Tweaks

--- Back-End/emailer.py
+++ Back-End/emailer.py
@@ -1,9 +1,11 @@
+import smtplib
 from datetime import date
 from email.message import EmailMessage
-import smtplib
+from os import remove
 
 from config import EMAIL_ADDRESS, EMAIL_PASSWORD
 from pdf_generator import create_pdf
+
 
 def send_email(data):
     today = date.today().strftime('%Y-%m-%d')
@@ -14,13 +16,15 @@ def send_email(data):
     msg['Subject'] = 'Attendance Report: ' + today
     msg.set_content('Please find the Attendance List for ' + today + ", of your class attached below\n- Verbose")
 
-    create_pdf(data["data"])
+    create_pdf(data["id"], data["data"])
 
-    with open("pdfTable.pdf", 'rb') as f:
+    with open("Student Daily Attendance Records.pdf", 'rb') as f:
         file_data = f.read()
 
-    msg.add_attachment(file_data, maintype = 'application', subtype = 'octet-stream', filename = data["id"] + " " + today)
+    msg.add_attachment(file_data, maintype = 'application', subtype = 'octet-stream', filename = data["id"] + " " + today + ".pdf")
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
+
+    remove("Student Daily Attendance Records.pdf")
