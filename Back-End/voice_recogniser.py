3aded27cc8fdc6d2ceaa0ff78098d838bc698204
Linukagoonetilleka <66565164+Linukagoonetilleka@users.noreply.github.com>
2021-04-23 03:59:30 +0530

    Initial Commits and Updates

--- Back-End/voice_recogniser.py
+++ Back-End/voice_recogniser.py
@@ -1,34 +1,73 @@
+from datetime import date
+from os import path, remove
+from tkinter import *
+
+from scipy.io.wavfile import write
+from sounddevice import rec, wait
 from speech_recognition import Microphone, Recognizer
+
 from blockchain import Blockchain
+from data_science import check_voice
+from server import get_ids, start_blockchain_functionality
 
-class VoiceRecogniser:
-    ids = ["w1761053", "w1761077", "w1790815"] #TODO: Integrate Teacher ID Module ASAP
-    blockchain_object = Blockchain()
+GUI = Tk()
+GUI.title("Voice Recorder")
+GUI.geometry("800x500")
+GUI.configure(background='#383b42')
 
-    blockchain_object.start_functionality()
+ids = get_ids(True)
+fs = 44100
+seconds = 10
+start_blockchain_functionality()
 
-    recognizer = Recognizer()
 
+variable1 = StringVar()
+firstLabel = Label(GUI, textvariable=variable1, relief=RAISED)
+firstLabel.pack(pady=30)
+variable1.set("Please click the button and vocalize your student ID, wait for 3 seconds and start speaking until attendance is marked")
+
+
+def record_function():
+    recordingButton.config(text="Recording audio.....")
     with Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source, duration=1)
+        Recognizer().adjust_for_ambient_noise(source, duration=1)
         print("\nPlease Vocalise Your Student ID: ")
-        # audio = recognizer.listen(source, timeout=1, phrase_time_limit=10)
-        audio = recognizer.listen(source)
-        
+        audio = Recognizer().listen(source)
+
         try:
-            text = recognizer.recognize_google(audio)
-            edited = text.replace(" ", "").lower()
-            print(edited)
+            text = Recognizer().recognize_google(audio)
+            text = text.replace(" ", "").lower()
+            print(text + "\n")
 
-            if edited in ids:
-                blockchain_object.add_block(edited)
-                print("\nThe ID is Valid")
-                print( blockchain_object.chain[-1].to_string() )
+        except():
+            print("Error: The ID Could Not Be Recognised! Please Try Again!")
 
-            else:
-                print("The ID is Invalid")
-            
-            blockchain_object.end_functionality()
+    if text in ids:
+        print("Check-In Phrase: ")
+        variable1.set("Please keep speaking to mark your attendance")
+        record_phrase = rec(int(seconds * fs), samplerate=fs, channels=1)
+        wait()
 
-        except:
-            print("Error: The Speech Could Not Be Recognised")
+        print("\nThe ID is Valid")
+        file_name = path.abspath("Dataset/Test") + "/" + text + "-" + str(date.today()) + "-8.wav"
+        write(file_name, fs, record_phrase)
+
+        if check_voice():
+            Blockchain().add_block(id=text, hash=Blockchain().chain[-1].compute_hash())
+            variable1.set("Attendance has been marked, thank you")
+        else:
+            print("Error: The Voice Could Not Be Recognised! Please Try Again!")
+            variable1.set("Attendance has not been marked, please try again ")
+
+        remove(file_name)
+    else:
+        print("The ID is Invalid!")
+        variable1.set("The ID could not be recognized, press the button again to record ")
+
+
+recordButton = PhotoImage(file=r"Images/icon-speech.png")
+
+recordingButton = Button(GUI, image=recordButton, command=record_function, borderwidth=0)
+recordingButton.pack(pady=25)
+
+mainloop()
